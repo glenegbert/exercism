46c2070d2fbd61b39630b8222fcb65ca5b372319
Glen Egbert <glenegbert1@gmail.com>
2018-11-22 14:35:55 -0700

    grade school

---
+++ ruby/grade-school/grade_school.rb
@@ -0,0 +1,34 @@
+class School
+  def initialize
+    @store = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def students(grade)
+    @store[grade]
+  end
+
+  def students_by_grade
+    @store.reduce([]) do |accum, (grade,students)|
+      accum << { grade: grade, students: students }
+    end.sort_by {|grd| grd[:grade] }
+  end
+
+  def add(student, grade)
+    if no_students?(grade)
+      @store[grade] = [student]
+    else
+      add_student_alphabetically(grade, student)
+    end
+  end
+
+  private
+
+  def no_students?(grade)
+    @store[grade].length == 0
+  end
+
+  def add_student_alphabetically(grade, student)
+    i = (nxt = @store[grade].bsearch {|s| s > student}) ? @store[grade].index(nxt) : -1
+    @store[grade].insert(i, student)
+  end
+end
